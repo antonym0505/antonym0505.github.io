3dabdd811568737064967e4717d70b7883330913
brian djerf <brian@eaiti.com>
2026-01-05 11:25:49 -0500

    update resume, skills

--- src/App.tsx
+++ src/App.tsx
@@ -60,8 +60,38 @@ function App() {
         </div>
         {/* <a href="https://briandjerf.com/about">about</a> */}
         {/* <a href="https://briandjerf.com/contact">contact</a> */}
+        <p>frontend playground</p>
         <Link to="tee-times">tee times</Link>
-        <a href="">more soon...</a>
+        <p>full stack projects</p>
+        <Link
+          to="https://intermodal-frontend-production.up.railway.app"
+          target="_blank"
+          style={{
+            display: 'flex',
+            font: 'inherit',
+            alignItems: 'center',
+          }}
+        >
+          <span
+            style={{
+              flex: 1,
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+            }}
+          >
+            intermodal
+          </span>
+          <svg
+            height={24}
+            width={24}
+            viewBox="0 -960 960 960"
+            style={{ fill: 'currentcolor', flexShrink: 0 }}
+          >
+            <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z" />
+          </svg>
+        </Link>
+        <p>more soon...</p>
       </nav>
       <Outlet />
     </>
--- src/home.tsx
+++ src/home.tsx
@@ -103,6 +103,36 @@ const Home: FC = () => {
               </svg>
             </Link>
           </dd>
+          <dd>
+            <Link
+              to="https://github.com/antonym0505/antonym0505.github.io"
+              target="_blank"
+              style={{
+                display: 'flex',
+                font: 'inherit',
+                alignItems: 'center',
+              }}
+            >
+              <span
+                style={{
+                  flex: 1,
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                  whiteSpace: 'nowrap',
+                }}
+              >
+                github.com/antonym0505
+              </span>
+              <svg
+                height={24}
+                width={24}
+                viewBox="0 -960 960 960"
+                style={{ fill: 'currentcolor', flexShrink: 0 }}
+              >
+                <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z" />
+              </svg>
+            </Link>
+          </dd>
           <dt />
         </dl>
         <Link
@@ -139,9 +169,9 @@ const Home: FC = () => {
           <dt>backend</dt>
           <dd>
             <ul>
-              <li>java</li>
-              <li>spring&bull;jpa</li>
-              <li>sql</li>
+              <li>java&bull;spring&bull;jpa</li>
+              <li>python&bull;fastAPI&bull;</li>
+              <li>postgres&bull;sql</li>
             </ul>
           </dd>
           <dt>testing</dt>
@@ -150,6 +180,7 @@ const Home: FC = () => {
               <li>cypress</li>
               <li>jest&bull;rtl</li>
               <li>selenium</li>
+              <li>playwright</li>
             </ul>
           </dd>
         </dl>
